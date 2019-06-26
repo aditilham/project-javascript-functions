c60aa89844da4988e730401c8d9f5aa3b39d086d
aditilham <aditilham152@gmail.com>
2019-06-26 11:30:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function userName(name1, name2) {
+    alert(`Welcome, ${name1} ${name2}!`)
+}
+
+let firstName = prompt(`Input your first name`)
+let lastName = prompt(`Input your last name`)
+
+userName(firstName, lastName)
