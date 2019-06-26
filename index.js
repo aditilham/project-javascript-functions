8edb4c23f70a802144021916143d145d122f801b
aditilham <aditilham152@gmail.com>
2019-06-26 12:13:58 +0700

    adding user age

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ function userName(name1, name2) {
     alert(`Welcome, ${name1} ${name2}!`)
 }
 
+const checkAge = function(age) {
+    if (age >= 18) {
+        alert('You are old enough')
+    } else if (age < 18 && age >= 0) {
+        alert('You are still young')
+    } else {
+        alert('You are not born yet')
+    }
+}
+
+
 let firstName = prompt(`Input your first name`)
 let lastName = prompt(`Input your last name`)
+let userAge = parseInt(prompt('Input your age'))
 
 userName(firstName, lastName)
+checkAge(userAge)
